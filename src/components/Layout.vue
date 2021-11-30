76259d6a780f2017e2f70627aed3b0be11f73b8d
rarescosmin <rarescosmin97@gmail.com>
2021-11-30 18:08:11 +0200

    Initial commit

---
+++ src/components/Layout.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="">
+        <div class="row" style="margin-top: 4em;">
+            <div class="col-1">
+            </div>
+            <HibernationCalculator />
+        </div>
+    </div>
+</template>
+
+<script>
+import HibernationCalculator from './HibernationCalculator.vue';
+
+export default {
+    name: 'Layout',
+    components: {
+    HibernationCalculator,
+  },
+}
+</script>
