136d47d86abb0c6d920215eb326068d734abc634
rarescosmin <rarescosmin97@gmail.com>
2021-11-30 18:23:41 +0200

    Added hibernatio-calculator card title component

---
+++ src/components/HibernationCalculator/HibernationCalculatorCardTitle.vue
@@ -0,0 +1,16 @@
+<template>
+    <h5 class="card-header info-color white-text text-center py-4">
+        <strong>{{ firstTitle }}</strong> <br>
+        <strong>{{ secondTitle }}</strong>
+    </h5>
+</template>
+
+<script>
+export default {
+    name: 'HibernationCalculatorCardTitle',
+    props: {
+        firstTitle: String,
+        secondTitle: String
+    }
+}
+</script>
--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -1,15 +1,17 @@
 <template>
+    <!-- first div with empty class to be deleted -->
     <div class="">
         <div class="row" style="margin-top: 4em;">
-            <div class="col-1">
+            <div class="col-1" />
+            <div class="col-4">
+                <HibernationCalculator />
             </div>
-            <HibernationCalculator />
         </div>
     </div>
 </template>
 
 <script>
-import HibernationCalculator from './HibernationCalculator.vue';
+import HibernationCalculator from './HibernationCalculator/HibernationCalculator.vue';
 
 export default {
     name: 'Layout',
