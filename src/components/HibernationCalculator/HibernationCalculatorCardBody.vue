94e7e4801661d2f79034bec97018f0662c6c70e9
rarescosmin <rarescosmin97@gmail.com>
2021-12-01 12:23:17 +0200

    Added button component for hirbernation calculator

--- src/components/HibernationCalculator/HibernationCalculatorCardBody.vue
+++ src/components/HibernationCalculator/HibernationCalculatorCardBody.vue
@@ -7,15 +7,21 @@
             </div>
             <div class="md-form">
                 <label for="defaultWeight">Weight before hibernation (g)</label>
-                    <input type="number" step="0.001" id="defaultWeight" class="form-control" aria-label="Weight before hibernation (g)" required />
+                <input type="number" step="0.001" id="defaultWeight" class="form-control" aria-label="Weight before hibernation (g)" required />
             </div>
-            <button class="btn btn-outline-info btn-rounded btn-block my-4 waves-effect z-depth-0" type="submit" id="defaultSubmit">Compute</button>
+            <HibernationCalculatorComputeButton />
         </form>
     </div>
 </template>
 
 <script>
+import HibernationCalculatorComputeButton from './HibernationCalculatorComputeButton.vue';
+
+
 export default {
-    name: 'HibernationCalculatorCardBody'
+    name: 'HibernationCalculatorCardBody',
+    components: {
+        HibernationCalculatorComputeButton
+    }
 }
 </script>
---
+++ src/components/HibernationCalculator/HibernationCalculatorComputeButton.vue
@@ -0,0 +1,30 @@
+<template>
+    <button 
+        class="btn btn-outline-info btn-rounded btn-block my-4 waves-effect z-depth-0" 
+        type="submit" id="defaultSubmit" 
+        @click="computeButtonHandler()">
+        {{ buttonText }}
+    </button>
+</template>
+
+<script>
+/**
+ * Handler for the 'Compute' button 
+ */
+const computeButtonHandler = () => {
+    console.log('clicked');
+}
+
+export default {
+    name: 'HibernationCalculatorComputeButton',
+    props: {
+        buttonText: {
+           type: String,
+           default: 'COMPUTE'
+        }
+    },
+    methods: {
+        computeButtonHandler
+    }
+}
+</script>
