7ee22e582da0753786d227dec14184a4ee0ebccb
rarescosmin <rarescosmin97@gmail.com>
2021-12-23 19:54:20 +0200

    removed unused code

--- src/components/HibernationCalculator/HibernationCalculator.vue
+++ src/components/HibernationCalculator/HibernationCalculator.vue
@@ -38,84 +38,6 @@ export default {
         }
     },
     async created() {
-
-        // console.debug('Attempting web3 object creation');
-        // const web3 = new Web3(process.env.VUE_APP_WEB3_PROVIDER);
-        // console.debug('Web3 object aquired');
-        // console.dir(web3);
-
-        // console.debug('Logging web3 accounts')
-        // console.dir(web3.eth.getAccounts());
-
-        // console.debug('Display current account balance');
-        // await web3.eth.getBalance(process.env.VUE_APP_WEB3_ACCOUNT, (err, bal) => {
-        //     console.debug(bal);
-        // });
-
-        // console.debug('Fetching contract...');
-        // const contract = new web3.eth.Contract(HibernationCalculatorContractInterface.abi, process.env.VUE_APP_WEB3_CONTRACT_ADDRESS);
-        // console.dir(contract);
-
-        // console.debug('Fetching animal uuids');
-        // const animalUUIDs = await contract.methods.getAnimalUUIDs().call({ from: process.env.VUE_APP_WEB3_ACCOUNT }, (err, result) => {
-
-        //     if (err) {
-        //         alert('Failed to fetch animal uuids');
-        //     }
-
-        //     return result;
-        // });
-        // console.debug(animalUUIDs);
-
-        // console.debug('Fetching animals by uuid..');
-        // animalUUIDs.forEach(async (uuid, indexKey) => {
-        //     await contract.methods.animals(uuid).call( { from: process.env.VUE_APP_WEB3_ACCOUNT }, (err, result) => {
-        //         if (err) {
-        //             alert('Error getting animal by passing uuid');
-        //         }
-
-        //         const parsedResult = (({ uuid, weight, length, result, min, avg, max }) => ({ uuid, weight, length, result, min, avg, max }))(result);
-        //         console.debug(parsedResult);
-        //         this.hibernationInformationTableData.push({ ...parsedResult, indexKey });
-        //     });
-        // });
-
-        // const contract = new web3.eth.Contract(HibernationCalculatorContractInterface.abi, "0x51C5aD37028081C792c6C88E48B371D6ff22390f");
-
-        // console.log(contract.methods);
-        
-        // contract.methods.animalCount().call((err, result) => {
-        //     console.log('result ' + result);
-        // });
-
-        // contract.methods.addAnimal(
-        //     "3dfef05b-0b66-450c-a0c6-d3994866f6d5",
-        //     BigInt(12000000000000000000),
-        //     BigInt(11000000000000000000),
-        //     3,
-        //     BigInt(257658000000000000000),
-        //     BigInt(303611000000000000000),
-        //     BigInt(350494000000000000000)
-        // ).send({
-        //     from: "0x0ED75BE115c0D28b34A9E2a7EEB41eaCae3e078e",
-        //     gas: 3000000
-        // }).then(receipt => {
-        //     console.log(receipt.events.AnimalAdded);
-        // })
-
-        // contract.methods.animalCount().call((err, result) => {
-        //     console.log('result ' + result);
-        // });
-
-        // turtleStore.syncFromLocalStorage();
-        // const measurements = turtleStore.measurements();
-
-        // if (Object.keys(measurements).length > 0) {
-        //     Object.keys(measurements).forEach((item, indexKey) => {
-        //         this.hibernationInformationTableData.push({ ...measurements[item], indexKey });
-        //     });
-        // }
-
         this.hibernationInformationTableData = await getAnimals();
     },
 	methods: {
@@ -132,7 +54,7 @@ export default {
             this.resultWeight = weight;
             this.resultTextStyle = [resultStatusMapper[result].panel];
 
-            const addedAnimal = await addAnimal(min, max, avg, result, shellLength, weight);
+            await addAnimal(min, max, avg, result, shellLength, weight);
 
             this.hibernationInformationTableData = await getAnimals();
         },
